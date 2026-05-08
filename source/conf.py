61c1671b5ad3e7db01e5f1ca52cb3371b59e1d2f
Sarbadal Pal <sarbadal.pal@omc.com>
2026-05-08 20:21:56 +0530

    try to remove .html from url

--- source/conf.py
+++ source/conf.py
@@ -18,3 +18,16 @@ exclude_patterns = []
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
+html_theme_options = {
+    'display_version': False,  # For older theme versions
+    'version_selector': False, # For newer theme versions
+}
+ 
+html_static_path = ['_static']
+html_css_files = [
+    'custom.css',
+]
+
+html_show_sphinx = False
+html_copy_source = False
+
